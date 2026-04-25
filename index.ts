973cab7a104be8cc7bbe58b16a8ede56e6c8b3df
LemonLime <111824263+HappyLemonLime@users.noreply.github.com>
2026-04-25 16:04:15 -0700

    fixed yellow1 role ID and updated redis.json

--- index.ts
+++ index.ts
@@ -47,7 +47,7 @@ const corndog: Corndog = new Client({
 
 const { CLIENT_TOKEN, STATUS } = process.env
 
-corndog.login("token goes here lol")
+corndog.login(CLIENT_TOKEN)
 const extension = __filename.split(".").pop() === 'ts' ? '.ts' : '.js'
 
 corndog.commands = new Collection(); corndog.app = app;
